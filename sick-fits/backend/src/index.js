73c581d95ee8fa069498458f17b072befb97e1f4
Michael Brown <m1ch4@m1ch4-laptop.local>
2020-02-25 15:45:55 -0500

    First and second pass at backend
    Launched Prisma environment
    Stood up Yoga (GraphQL on Express) Server
    Connected Prisma and Yoga environments
    Loaded test data into schema and data model

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer.js');
+const db = require('./db');
+
+const server = createServer();
+
+// Action item: cookies from JWT via express middleware
+// Action item: populate current user via express middleware
+
+server.start(
+    {
+        cors: {
+            credentials: true, 
+            origin: process.env.FRONTEND_URL
+        }
+    }, 
+    dets => {console.log(`¡Server running on port https://localhost:${dets.port}!`);
+    }
+);
